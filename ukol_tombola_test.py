5a3ef4a61f728fb6ac0925be64b9af6fc181c08a
Helena Ch <ellenkadata@gmail.com>
2022-03-06 21:46:57 +0100

    Helena Ch. - test odevzdani ukolu

---
+++ ukol_tombola_test.py
@@ -0,0 +1,21 @@
+tombola = {
+    7: "Láhev kvalitního vína Château Headache",
+    15: "Pytel brambor z místního družstva",
+    23: "Čokoládový dort",
+    47: "Kniha o historii města",
+    55: "Šiška salámu",
+    67: "Vyhlídkový let balónem",
+    79: "Moderní televizor",
+    91: "Roční předplatné městského zpravodaje",
+    93: "Společenská hra Sázky a dostihy",
+}
+
+listek=input("Jake mas cislo listku?")
+listek=int(listek)
+if listek in tombola:
+      print(f"Vyhral jsi {tombola[listek]}")
+      tombola.pop(listek)
+else: 
+      print("Nic jsi nevyhral")
+
+print(tombola)
