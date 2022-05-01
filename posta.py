0ebe76866df5689127ce88be1ad2cc116857d873
HelChytilova <66533573+HelChytilova@users.noreply.github.com>
2022-05-01 10:49:18 +0200

    Delete posta.py

--- posta.py
+++
@@ -1,9 +0,0 @@
-import re
-with open('posta.html', encoding='utf-8') as vstup:
-     posta=vstup.read()
-     
-posta=posta.replace('\n','\s')
-seznam=re.sub('[\s\n\t]+',' ',posta)
-seznam_comp=re.compile('\d{3}\s\d{2}\s[a-žA-Ž\s\d]{1,}')
-vypis_seznam=seznam_comp.findall(seznam)
-print(vypis_seznam)
