94b63a9444aad840984d9ab9d35b950be3cdf24b
Helena Ch <ellenkadata@gmail.com>
2022-05-01 10:52:30 +0200

    ukol c.7 - posta

--- posta.py
+++ posta.py
@@ -1,7 +1,6 @@
 import re
 with open('posta.html', encoding='utf-8') as vstup:
      posta=vstup.read()
-     
 posta=posta.replace('\n','\s')
 seznam=re.sub('[\s\n\t]+',' ',posta)
 seznam_comp=re.compile('\d{3}\s\d{2}\s[a-žA-Ž\s\d]{1,}')
